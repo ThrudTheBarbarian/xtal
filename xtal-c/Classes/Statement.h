5a6c1a69f7c1adb1743d5534217f0e1463a53742
ThrudTheBarbarian <github@gornall.net>
2022-11-26 17:43:54 -0800

    Checkpoint: moving towards variables

---
+++ xtal-c/Classes/Statement.h
@@ -0,0 +1,38 @@
+//
+//  Statement.h
+//  xtal-c
+//
+//  Created by Simon Gornall on 11/26/22.
+//
+
+#ifndef Statement_h
+#define Statement_h
+
+#include <cstdio>
+#include <string>
+
+#include "properties.h"
+#include "macros.h"
+
+class Statement
+	{
+    NON_COPYABLE_NOR_MOVEABLE(Statement)
+ 
+	/************************************************************************\
+    |* Properties
+    \************************************************************************/
+    
+    private:
+        
+    public:
+        /********************************************************************\
+        |* Constructors and Destructor
+        \********************************************************************/
+        explicit Statement();
+
+        /********************************************************************\
+        |* 
+        \********************************************************************/
+	};
+
+#endif /* Statement_h */
