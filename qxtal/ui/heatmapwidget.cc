6f11f75dd594b19208e774cc6062571588e27771
ThrudTheBarbarian <github@gornall.net>
2023-01-14 17:47:07 -0800

    Start getting the heatmaps in place

--- qxtal/ui/heatmapwidget.cc
+++ qxtal/ui/heatmapwidget.cc
@@ -1,6 +1,5 @@
 #include "heatmapwidget.h"
 
-#include <QPainter>
 #include <QColor>
 
 /*****************************************************************************\
@@ -9,7 +8,9 @@
 HeatMapWidget::HeatMapWidget(QWidget *parent)
 			  :QWidget{parent}
 	{
-
+	memset(_wr, 0x00, sizeof(_wr));
+	memset(_pc, 0x00, sizeof(_pc));
+	memset(_rd, 0x00, sizeof(_rd));
 	}
 
 /*****************************************************************************\
@@ -17,7 +18,35 @@ HeatMapWidget::HeatMapWidget(QWidget *parent)
 \*****************************************************************************/
 void HeatMapWidget::paintEvent(QPaintEvent *)
 	{
+	/*************************************************************************\
+	|* Paint the background
+	\*************************************************************************/
+	QRect bounds = rect();
+	bounds.adjust(0,0,-1,-1);
+
 	QPainter painter(this);
-	painter.setBrush(QColor(0, 0, 255, 127));
-	painter.fillRect(rect(), painter.brush());
+	painter.setBrush(QColor(255,255,255,255));
+	painter.fillRect(bounds, painter.brush());
+	painter.setPen(QColor(0,0,0,255));
+	painter.drawRect(bounds);
+
+	_heatmap(painter, _wr, 400);
+	_heatmap(painter, _pc, 700);
+	}
+
+/*****************************************************************************\
+|* Paint a heatmap
+\*****************************************************************************/
+void HeatMapWidget::_heatmap(QPainter &P, uint32_t *data, int y)
+	{
+	int w = 256;
+	int h = 256;
+	int x = (rect().width() - w) / 2;
+
+	P.setPen(QColor(0,0,0,255));
+	P.drawRect(x,y,w+2,h+2);
+	x++;
+	y++;
+
+
 	}
--- qxtal/ui/heatmapwidget.h
+++ qxtal/ui/heatmapwidget.h
@@ -2,11 +2,29 @@
 #define HEATMAPWIDGET_H
 
 #include <QObject>
+#include <QPainter>
 #include <QWidget>
+
+#include "sim/atari.h"
 
 class HeatMapWidget : public QWidget
 	{
 	Q_OBJECT
+
+	/*************************************************************************\
+	|* Properties
+	\*************************************************************************/
+	GET(Atari*, hw);				// Hardware being simulated
+
+	private:
+		uint32_t _rd[65536];		// Read operation heat-map
+		uint32_t _wr[65536];		// Write operation heat-map
+		uint32_t _pc[65536];		// PC heat-map
+
+		/*********************************************************************\
+		|* Paint a heatmap
+		\*********************************************************************/
+		void _heatmap(QPainter &P, uint32_t *data, int y);
 
 	public:
 		explicit HeatMapWidget(QWidget *parent = nullptr);
